25a88ed508d5ffa5d02081d00b67f2c04019c5b4
kimda-bin <rlaekqls0504@gmail.com>
2025-02-05 22:17:40 +0900

    favorite button add,remove

--- client/src/components/views/MovieDetail/Section/Favorite.js
+++ client/src/components/views/MovieDetail/Section/Favorite.js
@@ -1,5 +1,6 @@
 import Axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Button } from "antd";
 
 function Favorite(props) {
   const movieId = props.movieId;
@@ -8,22 +9,68 @@ function Favorite(props) {
   const moviePost = props.movieInfo.backdrop_path;
   const movieRunTime = props.movieInfo.runtime;
 
-  useEffect(() => {
-    let variables = {
-      userFrom,
-      movieId,
-    };
+  const [FavoriteNumber, setFavoriteNumber] = useState(0);
+  const [Favorited, setFavorited] = useState(false);
+  let variables = {
+    userFrom: userFrom,
+    movieId: movieId,
+    movieTitle: movieTitle,
+    moviePost: moviePost,
+    movieRunTime: movieRunTime,
+  };
 
+  useEffect(() => {
     Axios.post("/api/favorite/favoriteNumber", variables).then((response) => {
       console.log(response.data);
       if (response.data.success) {
+        setFavoriteNumber(response.data.favoriteNumber);
       } else {
         alert("숫자 정보를 가져오는데 실패 했습니다.");
       }
     });
+
+    Axios.post("/api/favorite/favorited", variables).then((response) => {
+      console.log(response.data);
+      if (response.data.success) {
+        setFavorited(response.data.favorited);
+        console.log("favorited", response.data);
+      } else {
+        alert("정보를 가져오는데 실패 했습니다.");
+      }
+    });
   }, []);
 
-  return <button>Favorite</button>;
+  const onClickFavorite = () => {
+    if (Favorited) {
+      Axios.post("/api/favorite/removeFromFavorite", variables).then(
+        (response) => {
+          if (response.data.success) {
+            setFavoriteNumber(FavoriteNumber - 1);
+            setFavorited(!Favorited);
+          } else {
+            alert("Favorite 리스트에서 지우는 걸 실패했습니다");
+          }
+        }
+      );
+    } else {
+      Axios.post("/api/favorite/addToFavorite", variables).then((response) => {
+        if (response.data.success) {
+          setFavoriteNumber(FavoriteNumber + 1);
+          setFavorited(!Favorited);
+        } else {
+          alert("Favorite 리스트에 추가하는 걸 실패했습니다");
+        }
+      });
+    }
+  };
+
+  return (
+    <div>
+      <Button onClick={onClickFavorite}>
+        {Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber}
+      </Button>
+    </div>
+  );
 }
 
 export default Favorite;
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -16,4 +16,43 @@ router.post("/favoriteNumber", (req, res) => {
   });
 });
 
+//내가 이 영화를 Favorite 리스트에 넣었는지 정보를 DB에서 가져오기
+router.post("/favorited", (req, res) => {
+  //mongoDB에서 favorite 숫자를 가져오기
+  Favorite.find({
+    movieId: req.body.movieId,
+    userFrom: req.body.userFrom,
+  }).exec((err, info) => {
+    if (err) return res.status(400).send(err);
+
+    let result = false;
+    if (info.length !== 0) {
+      result = true;
+    }
+
+    //그다음에 프론트에 다시 숫자 정보를 보내주기
+    res.status(200).json({ success: true, favorited: result });
+  });
+});
+
+router.post("/removeFromFavorite", (req, res) => {
+  Favorite.findOneAndDelete({
+    movieId: req.body.movieId,
+    userFrom: req.body.userFrom,
+  }).exec((err, doc) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).json({ success: true, doc });
+  });
+});
+
+router.post("/addToFavorite", (req, res) => {
+  //인스턴스 생성
+  const favorite = new Favorite(req.body);
+  //Favorite 안에 넣어준 값들을 저장시킴
+  favorite.save((err, doc) => {
+    if (err) return res.status(400).send(err);
+    return res.status(200).json({ success: true });
+  });
+});
+
 module.exports = router;
